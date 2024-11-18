8a3da5cb879dcc079801309d7b1efc2eddec4204
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-18 16:34:03 +0800

    fix apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           </div>
           <div className="space-y-4 md:w-1/2 md:space-y-8">
             <p className="text-lg md:text-xl">
-              We're a family-owned business bringing the authentic taste of
+              We&apos;re a family-owned business bringing the authentic taste of
               Malaysia to Dunedin. Kampung means village in Malay, symbolizing
               warmth, tradition, and family. We hope these flavors will bring
               you as much joy as they bring us.
@@ -101,12 +101,12 @@ export default function Home() {
           <div className="flex flex-col justify-between gap-x-8 gap-y-4 md:flex-row">
             <Note>
               <h4 className="text-xl font-medium lg:text-2xl">
-                And there’s more to come!
+                And there&apos;s more to come!
               </h4>
               <p className="text-lg lg:text-xl">
-                We’re trialing exciting new dishes each week to bring you even
-                more flavors of Malaysia. Stay tuned for updates on our weekly
-                menus!
+                We&apos;re trialing exciting new dishes each week to bring you
+                even more flavors of Malaysia. Stay tuned for updates on our
+                weekly menus!
               </p>
             </Note>
             <div className="flex items-center justify-end">
@@ -129,8 +129,9 @@ export default function Home() {
             <p className="text-lg lg:text-xl">
               Craving a taste of home or curious about Malaysian cuisine? Place
               your order now and let Kampung Dunedin bring the deliciousness to
-              your doorstep. We're here to make your meals special, whether it's
-              a quiet weekend at home or a celebration with loved ones!
+              your doorstep. We&apos;re here to make your meals special, whether
+              it&apos;s a quiet weekend at home or a celebration with loved
+              ones!
             </p>
             <ul className="text-lg lg:text-xl">
               Delivery info:
@@ -156,10 +157,10 @@ export default function Home() {
               <p className="text-lg lg:text-xl">
                 Planning an event or feeding a{" "}
                 <span className="font-semibold">larger group</span>? Kampung
-                Dunedin is happy to cater to your needs! Whether it's a family
-                gathering, office party, or special celebration, we'll craft a
-                menu to delight your guests with authentic Malaysian flavors.
-                Get in touch for custom catering options.
+                Dunedin is happy to cater to your needs! Whether it&apos;s a
+                family gathering, office party, or special celebration,
+                we&apos;ll craft a menu to delight your guests with authentic
+                Malaysian flavors. Get in touch for custom catering options.
               </p>
             </Note>
           </div>
@@ -172,9 +173,9 @@ export default function Home() {
         </Headingwbackground>
         <p className="text-lg lg:text-xl">
           Our menu changes weekly, so join our WhatsApp group or follow us on
-          Instagram for the latest updates and to see what's cooking each
-          weekend! We'd love for you to be part of our community and share in
-          the flavors of Kampung Dunedin.
+          Instagram for the latest updates and to see what&apos;s cooking each
+          weekend! We&apos;d love for you to be part of our community and share
+          in the flavors of Kampung Dunedin.
         </p>
         <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
           <Button size="lg" variant="outline">
