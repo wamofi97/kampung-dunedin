1c8baf2156a066964ac8d9ac05bc448a6edd2d94
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-20 19:28:35 +0800

    update sizing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,17 +4,17 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="mx-auto flex w-full max-w-4xl flex-col items-center px-3 py-8 md:px-6">
+    <div className="mx-auto flex w-full max-w-6xl flex-col items-center px-3 py-12 md:px-6">
       <h1 className="text-3xl">Our Story</h1>
       <Headingwbackground>
         From Malaysia to Dunedin – Sharing the Flavors of Home
       </Headingwbackground>
 
-      <p className="my-4 text-lg md:text-xl">
+      <p className="mb-4 md:text-lg">
         Welcome to Kampung Dunedin, where the flavors of Malaysia meet the heart
         of Dunedin, New Zealand.
       </p>
-      <p className="my-4 text-lg md:text-xl">
+      <p className="my-4 md:text-lg">
         We are a small, family-owned food business with a passion for sharing
         authentic Malaysian cuisine. Our journey began with a simple longing for
         home — the familiar taste of spices, the aroma of pandan leaves, and the
@@ -29,45 +29,45 @@ const About = () => {
           className="rounded-lg object-cover"
         ></Image>
       </div>
-      <p className="my-4 text-lg md:text-xl">
+      <p className="my-4 md:text-lg">
         Being away from our kampung (village) has taught us the true meaning of
         comfort food. It&apos;s not just about the taste; it&apos;s about the
         memories, the togetherness, and the stories shared over a meal.
-        That&apos;s why we started Kampung Dunedin: to bring a piece of our
+        That&apos;s why we started Kampung Dunedin, to bring a piece of our
         culture, traditions, and love for food to this beautiful city.
       </p>
-      <p className="my-4 text-lg md:text-xl">
+      <p className="my-4 md:text-lg">
         Every weekend, we prepare dishes that hold a special place in our
         hearts. From the iconic Nasi Lemak to the smoky, flavorful Sate Ayam and
         Sate Daging, our menu reflects the diverse culinary heritage of
         Malaysia. Each recipe is crafted with care, using authentic ingredients
         and techniques passed down through generations.
       </p>
-      <p className="my-4 text-lg md:text-xl">
+      <p className="my-4 md:text-lg">
         But Kampung Dunedin is more than just a food business — it&apos;s a way
         to build connections. Through our food, we hope to share a taste of
         Malaysia with you, sparking joy and creating new memories around your
         dining table. Whether you&apos;re new to Malaysian cuisine or already a
         fan, we invite you to experience the magic of kampung-style cooking.
       </p>
-      {/* <p className="my-4 text-lg md:text-xl ">
+      {/* <p className="my-4 md:text-lg ">
         We're always experimenting with flavors and dishes, introducing new menu
         items every week. Our dream is to not only satisfy your cravings but
         also to build a community of food lovers who appreciate the richness of
         Malaysian culture.
       </p> */}
-      <p className="my-4 text-lg md:text-xl">
+      <p className="my-4 md:text-lg">
         Thank you for supporting our journey. From our kitchen to your table, we
         hope to bring you comfort, warmth, and a whole lot of deliciousness.
-        Selamat menjamu selera! (Enjoy your meal!) 🥰
+        Selamat menjamu selera! (Enjoy your meal! 💓)
       </p>
-      {/* <p className="my-4 text-lg md:text-xl ">
+      {/* <p className="my-4 md:text-lg ">
         As a family, we began recreating the meals that reminded us of home -
         dishes that brought us together and filled our hearts as much as our
         stomachs. These recipes, passed down through generations, became our
         little sanctuary of flavor, even when we were miles away from Malaysia.
       </p>
-      <p className="my-4 text-lg md:text-xl ">
+      <p className="my-4 md:text-lg ">
         Now, we want to share these dishes with you - our neighbors in Dunedin.
         'Kampung' means 'village' in Malay, symbolizing family, community, and
         tradition. Through Kampung Dunedin, we hope to connect with you and
