c4e0d91b7df17a6795f2f95e21e3e4675be41017
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-20 19:27:44 +0800

    refactor code and add icon for mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,37 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import { Menu, X } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const navLinks = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Menu",
+      link: "/menu",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Review",
+      link: "/review",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
+  const pathname = usePathname();
 
   return (
-    <nav className="sticky top-0 z-20 flex w-full justify-center overflow-x-hidden">
+    <nav className="sticky top-0 z-20 flex w-full justify-center overflow-x-hidden bg-lime-900">
       <div className="flex w-full max-w-7xl items-center justify-between px-6 py-4">
         <Link href={"/"}>
           <Image
@@ -17,96 +42,56 @@ const Navbar = () => {
             alt="Logo"
             width={100}
             height={100}
-            className="w-32"
+            className="w-32 invert"
           />
         </Link>
-        <ul className="hidden gap-8 md:flex">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/review">Review</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
+        <div className="flex gap-8 font-heading uppercase text-white lg:gap-12">
+          {navLinks.map((link) => (
+            <Link
+              key={link.name}
+              href={link.link}
+              className={`${pathname === link.link ? "font-bold text-primary" : "font-normal"} hidden text-lg transition-colors duration-300 hover:text-amber-400 md:block`}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
         <a href="https://bit.ly/WhatsAppKampungDunedin" target="_blank">
           <Button className="hidden md:block">Order Now</Button>
         </a>
         <button
-          className="z-50 block md:hidden"
+          className="z-50 block text-primary md:hidden"
           onClick={() => {
             setIsOpen(!isOpen);
           }}
         >
-          Menu
+          {isOpen ? <X className="h-8 w-8" /> : <Menu className="h-8 w-8" />}
         </button>
 
         <div
-          className={`${!isOpen && "translate-x-full"} fixed right-0 top-0 h-screen w-2/3 bg-neutral-100/95 p-4 py-20 text-2xl shadow transition-all duration-500 sm:w-1/3 md:hidden`}
+          className={`${!isOpen && "translate-x-full"} fixed right-0 top-20 h-screen w-2/3 space-y-20 bg-neutral-100/95 p-4 py-32 text-2xl shadow transition-all duration-500 sm:w-1/3 md:hidden`}
         >
-          <ul className="flex h-full flex-col items-center justify-center gap-12">
-            <li>
+          <div className="flex flex-col items-center gap-12">
+            {navLinks.map((link) => (
               <Link
-                href="/"
+                key={link.name}
+                href={link.link}
+                className={`${pathname === link.link && "font-semibold text-primary"} w-fit`}
                 onClick={() => {
                   setIsOpen(!isOpen);
                 }}
               >
-                Home
+                {link.name}
               </Link>
-            </li>
-            <li>
-              <Link
-                href="/menu"
-                onClick={() => {
-                  setIsOpen(!isOpen);
-                }}
-              >
-                Menu
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                onClick={() => {
-                  setIsOpen(!isOpen);
-                }}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/review"
-                onClick={() => {
-                  setIsOpen(!isOpen);
-                }}
-              >
-                Review
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/contact"
-                onClick={() => {
-                  setIsOpen(!isOpen);
-                }}
-              >
-                Contact
-              </Link>
-            </li>
+            ))}
+          </div>
+          <div className="flex justify-center">
             <a href="https://bit.ly/WhatsAppKampungDunedin" target="_blank">
-              <Button className="">Order Now</Button>
+              <Button size="lg" className="">
+                Order Now
+              </Button>
             </a>
-          </ul>
+          </div>
         </div>
       </div>
     </nav>
