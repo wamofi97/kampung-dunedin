e9a9627b68f1a87b656cae4acc4f7533548ad2b9
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-21 14:24:00 +0800

    update text color

--- src/components/Headingwbackground.tsx
+++ src/components/Headingwbackground.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Headingwbackground = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mb-8 w-full rounded-xl py-3 text-center uppercase md:mb-16">
+    <div className="mb-8 w-full rounded-xl py-3 text-center uppercase text-emerald-950/80 md:mb-16">
       <h2 className="font-heading text-3xl font-bold md:text-4xl lg:text-5xl">
         {children}
       </h2>
