936dde57578a682d7fec6720bb7e805e24b1d1b7
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-21 14:25:06 +0800

    udpate text-colors and spacing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,20 +6,13 @@ import MenuSlider from "./MenuSlider";
 
 const Hero = () => {
   return (
-    <div className="z-10 flex min-h-[80vh] w-full max-w-7xl flex-col items-center justify-center gap-2 sm:min-h-screen">
-      <div className="flex flex-col items-center space-y-4 md:w-4/5">
-        <Image
-          src="/whitelogo.png"
-          alt="Logo"
-          width="180"
-          height="120"
-          className="mx-auto block object-cover invert lg:hidden"
-        />
-        <h2 className="hidden text-center text-sm text-muted-foreground md:block md:text-xl">
+    <div className="z-10 flex min-h-[80vh] w-full max-w-7xl flex-col items-center justify-center gap-2 md:min-h-screen">
+      <div className="flex flex-col items-center gap-y-6 md:w-4/5">
+        <h2 className="rounded-full bg-amber-100 px-2 py-1 text-center text-sm text-muted-foreground md:px-4 md:text-base">
           Authentic Malaysian Food in Dunedin
         </h2>
-        <div className="flex flex-col items-center justify-between lg:flex-row">
-          <div className="hidden w-1/4 lg:block">
+        <div className="flex flex-col items-center justify-between sm:flex-row">
+          <div className="md:w-1/4">
             <Image
               src="/whitelogo.png"
               alt="Logo"
@@ -28,25 +21,17 @@ const Hero = () => {
               className="mx-auto object-cover invert"
             />
           </div>
-          <h1 className="w-fit text-center font-heading text-[1.6rem] font-bold sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
-            Bringing a Taste of{" "}
-            <span className="bg-gradient-to-t from-emerald-900 to-amber-400 bg-clip-text text-transparent">
-              Malaysia
-            </span>{" "}
-            to Dunedin.
-            {/* <span className="bg-gradient-to-t from-sky-800 to-sky-300 bg-clip-text font-black text-transparent">
-              {" "}
-              Dunedin!
-            </span> */}
+          <h1 className="w-fit text-center font-heading text-[1.6rem] font-bold text-emerald-950 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+            Bringing a Taste of <span className="">Malaysia</span> to Dunedin.
           </h1>
         </div>
 
-        <p className="text-center text-lg md:text-xl lg:text-3xl">
+        <p className="text-center text-lg md:text-xl lg:text-2xl">
           A family-owned food business serving authentic Malaysian dishes to
           your doorstep, every weekend.
         </p>
 
-        <div className="my-4 flex justify-center gap-2 sm:hidden md:gap-4">
+        <div className="flex justify-center gap-2 sm:hidden md:gap-4">
           <Link href="/menu">
             <Button variant="outline">Explore our menu</Button>
           </Link>
