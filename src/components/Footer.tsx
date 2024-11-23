efa193992391d3d3ffa189118938c5bf6507d55f
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-23 15:11:29 +0800

    update text color and remove phone option

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bottom-0 mt-24 flex w-full justify-center border-t bg-emerald-900 text-white/80">
+    <footer className="bottom-0 mt-24 flex w-full justify-center border-t bg-emerald-900 text-accent">
       <div className="flex w-full max-w-6xl items-center justify-between gap-20 px-3 py-6 md:px-6">
         <div>
           <p className="text-sm">
-            {" "}
             Copyright &copy; 2024 Kampung Dunedin. All Rights Reserved.
           </p>
           <p className="text-sm text-gray-400">Developed by by wamofi.dev</p>
@@ -21,13 +20,6 @@ const Footer = () => {
           >
             <MessageSquare className="h-5 w-5" />
           </a>
-          <a
-            href="tel:021-123-4567"
-            className="flex items-center gap-3 transition-colors hover:text-primary"
-          >
-            <Phone className="h-5 w-5" />
-          </a>
-
           <a
             href="https://instagram.com/kampungdunedin"
             target="_blank"
