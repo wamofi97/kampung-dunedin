73265e41a99ec541a265c71bb492d5d3972d3d6a
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-23 15:13:00 +0800

    add classname as props

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { cn } from "@/lib/utils";
+// import clsx from "clsx";
 
-const Note = ({ children }: { children: React.ReactNode }) => {
+const Note = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className="mx-auto w-fit rounded-lg border-0 border-l-8 border-primary bg-gradient-to-r from-background to-accent px-8 py-8">
-      <div className="">{children}</div>
+    <div
+      className={cn(
+        className,
+        "mx-auto w-fit rounded-lg border-l-8 border-primary bg-gradient-to-r from-background to-accent px-8 py-8",
+      )}
+    >
+      {children}
     </div>
   );
 };
