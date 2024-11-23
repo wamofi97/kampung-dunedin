71e80b4eb15f8d97b106e5df97eadb169ee352b1
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-24 01:34:34 +0800

    adjust spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,12 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bottom-0 mt-24 flex w-full justify-center border-t bg-emerald-900 text-accent">
-      <div className="flex w-full max-w-6xl items-center justify-between gap-20 px-3 py-6 md:px-6">
-        <div>
-          <p className="text-sm">
-            Copyright &copy; 2024 Kampung Dunedin. All Rights Reserved.
-          </p>
-          <p className="text-sm text-gray-400">Developed by by wamofi.dev</p>
+      <div className="flex w-full max-w-6xl items-center justify-between gap-6 px-3 py-6 md:px-6">
+        <div className="text-[0.75rem] md:text-sm">
+          <p>Copyright &copy; 2024 Kampung Dunedin. All Rights Reserved.</p>
+          <p className="text-gray-400">Developed by by wamofi.dev</p>
         </div>
-        <div className="flex flex-wrap gap-6">
+        <div className="flex gap-4 md:gap-6">
           <a
             href="https://bit.ly/WhatsAppKampungDunedin"
             target="_blank"
