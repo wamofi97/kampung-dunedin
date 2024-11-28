879a578607cc253fb14c9dca43b3ab26ce0b2d52
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-29 07:56:27 +0800

    remove dotted border

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -12,7 +12,7 @@ const Note = ({
   return (
     <div
       className={cn(
-        "mx-auto rounded-lg border-2 border-dotted border-primary bg-amber-100/50 px-6 py-6 text-foreground/80",
+        "mx-auto rounded-lg border-l-8 border-primary bg-white/50 px-6 py-6 text-foreground/80 shadow-md",
         className,
       )}
     >
