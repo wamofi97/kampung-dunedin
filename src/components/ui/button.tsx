1167ed9b29b96f6d9599a262aa3b72e30c84514d
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-11-28 22:22:21 +0800

    update button secondary

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-accent hover:bg-accent/80  hover:text-secondary",
         secondary:
-          "border border-secondary text-primary hover:text-accent bg-emerald-800 hover:bg-secondary/90",
+          "border border-secondary text-accent hover:text-primary bg-emerald-800 hover:bg-secondary/90",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
