2f9ddc73b343c981800d58245621524d190e6bca
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-12-03 06:23:15 +0800

    update reviews

--- src/app/review.ts
+++ src/app/review.ts
@@ -9,46 +9,22 @@ export type Review = {
 export const reviews: Review[] = [
   {
     id: 1,
-    author: "Aisyah J.",
-    rating: 4,
-    text: "The Nasi Lemak Daun Pisang is absolutely divine! It took me straight back to my childhood in Malaysia. Thank you for sharing this amazing dish!",
-  },
-  {
-    id: 2,
-    author: "Michael T.",
-    rating: 4,
-    text: "The Sate Ayam is so tender and flavorful, and the peanut sauce is spot on. My kids loved it too!",
-  },
-  {
-    id: 3,
-    author: "Daniel K",
-    rating: 5,
-    text: "Tried the Nasi Kandar Daging Masak Hitam Mamak last week – everything blended so perfectly. Feels like I’ve been transported to Penang!",
-  },
-  {
-    id: 4,
-    author: "Jonathan L.",
-    rating: 5,
-    text: "Kampung Dunedin has truly brought a taste of Malaysia to NZ. Every bite reminded me of my travels to Southeast Asia.",
-  },
-  {
-    id: 5,
-    author: "Nurul H.",
-    rating: 5,
-    text: "The Sate Ayam is so tender and flavorful, and the peanut sauce is spot on. My kids loved it too!",
-  },
-  {
-    id: 6,
     author: "Siti Roziana",
     desc: "Malaysian working in Dunedin",
     rating: 5,
     text: "I have zero regrets trying each item on their menu because it was beyond delicious. The spiciness was just right - mild enough for someone like me to enjoy, but with just the right amount of kick from the spices to bring out those flavors. This means both those with low spice tolerance and those who enjoy extra heat in their food can enjoy it!",
   },
   {
-    id: 7,
+    id: 2,
     author: "Richard Knights",
-    desc: "Malaysian working in Dunedin",
+    desc: "Admin of What's News Dunedin Group & Programme Host",
     rating: 5,
-    text: "Got to try the beef and chicken satay and it was ALL kinds of yum…loved the peanut satay sauce coming in a cute little bag too…was yummy and left me wanting more…the beef was my fave 😋",
+    text: "Got to try the beef and chicken satay and it was ALL kinds of yum.. Loved the peanut satay sauce coming in a cute little bag too - was yummy and left me wanting more. The beef was my fave 😋",
+  },
+  {
+    id: 3,
+    author: "Tayla Simpson",
+    rating: 5,
+    text: "I really really loved the peanuts in the nasi lemak, they were so addicting! As for the rendang, it was so beautiful and I loved how much depth there was - layers of flavours. Also, the pressed rice was so fun to eat texture wise! ",
   },
 ];
--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,6 +1,6 @@
 import Headingwbackground from "@/components/Headingwbackground";
 import { Button } from "@/components/ui/button";
-import { Star } from "lucide-react";
+import { Quote, Star } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { reviews } from "@/app/review";
@@ -11,21 +11,23 @@ const Review = () => {
       <div className="dotted-background"></div>
       <Headingwbackground>What Our Customers Are Saying</Headingwbackground>
 
-      <div className="mb-20 max-w-4xl space-y-8">
+      <div className="mb-20 flex max-w-4xl flex-col gap-y-8">
         {reviews.map((review) => (
           <div
             key={review.id}
-            className="item-center mb-8 flex flex-col gap-6 rounded-lg border-b p-8 text-center"
+            className="item-center relative flex min-w-[500px] flex-col gap-4 rounded-lg border-b p-8 text-center"
           >
+            <Quote className="absolute left-1/2 top-1/2 h-16 w-16 translate-x-[-50%] translate-y-[-50%] opacity-5" />
+            {/* <Quote className="absolute bottom-8 left-8 h-12 w-12 rotate-180 opacity-10" /> */}
+            <p className="font-regular mb-8 font-heading text-lg text-neutral-600">{`"${review.text}"`}</p>
             <div className="mx-auto flex items-center">
               {[...Array(review.rating)].map((_, index) => (
                 <Star
                   key={index}
-                  className="h-3 w-3 fill-yellow-400 text-yellow-400"
+                  className="h-3 w-3 fill-amber-200 text-amber-200"
                 ></Star>
               ))}
             </div>
-            <p className="italic text-neutral-500">{`"${review.text}"`}</p>
             <p className="font-medium">{review.author}</p>
           </div>
         ))}
