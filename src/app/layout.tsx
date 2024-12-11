8a3e77805f82efd70d79344c5c8e65897b6ce8d3
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-12-12 05:00:43 +0800

    test remove link icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,15 +51,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" href="/favicon.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
         <Script id="structured-data" type="application/ld+json">
           {JSON.stringify({
             "@context": "https://schema.org",
