30ff586f252bde21a23977a47977620048125da7
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-12-26 09:23:44 +0800

    add spinner for image loading

--- src/components/DialogMenu.tsx
+++ src/components/DialogMenu.tsx
@@ -2,7 +2,7 @@
 import { X } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface MenuItem {
   menu: {
@@ -31,13 +31,15 @@ const DialogMenu = ({ menu }: MenuItem) => {
     };
   }, []);
 
+  const [loading, setLoading] = useState(true);
+
   return (
     <div
       className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 px-2"
       onClick={closeModal}
     >
       <dialog
-        className="relative w-full max-w-5xl rounded-md bg-white p-2"
+        className="relative w-full max-w-6xl rounded-md bg-white p-2"
         onClick={(e) => e.stopPropagation()}
         open
       >
@@ -49,8 +51,13 @@ const DialogMenu = ({ menu }: MenuItem) => {
         </button>
         <div className="flex max-h-[85vh] flex-col items-center gap-x-4 overflow-auto sm:flex-row">
           <div
-            className={`relative h-[50vh] min-h-[350px] w-full sm:h-[70vh] sm:w-3/5`}
+            className={`relative h-[50vh] min-h-[350px] w-full sm:h-[80vh] sm:w-3/4 md:w-3/5`}
           >
+            {loading && (
+              <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
+                <div className="flex h-10 w-10 animate-spin items-center justify-center rounded-full border-4 border-t-secondary"></div>
+              </div>
+            )}
             <Image
               src={menu.imageUrl}
               alt={menu.altText || menu.name}
@@ -58,13 +65,15 @@ const DialogMenu = ({ menu }: MenuItem) => {
               placeholder="blur"
               blurDataURL={menu.blurDataURL}
               quality={100}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 5
+              0vw"
+              onLoad={() => setLoading(false)}
               className={`${
                 menu.name.startsWith("Sate") && "object-bottom"
               } h-auto w-auto object-cover`}
             />
           </div>
-          <div className="sm:w-2/5">
+          <div className="sm:w-1/4 md:w-2/5">
             <h2 className="my-2 text-center font-heading text-xl font-semibold uppercase text-secondary sm:my-4 sm:text-3xl md:mb-4 md:text-4xl lg:text-[2.75rem] lg:leading-none xl:text-5xl">
               {menu.name}
             </h2>
