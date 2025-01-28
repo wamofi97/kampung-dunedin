aa94da240fd22deecc7bcad4344e987aa7cd1995
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-01-29 00:25:58 +0800

    add responsive close modal

--- src/components/DialogMenu.tsx
+++ src/components/DialogMenu.tsx
@@ -63,17 +63,24 @@ const DialogMenu = ({ menu, ids }: { menu: MenuItem; ids: string[] }) => {
       className={`fixed inset-0 z-50 flex items-center justify-center bg-black/80 px-2 md:px-12`}
       onClick={closeModal}
     >
-      <dialog className="relative w-full max-w-6xl bg-transparent" open>
+      <button
+        onClick={closeModal}
+        className="group absolute right-2 top-2 z-10 flex items-center justify-center rounded-full bg-gray-800/80 p-2 text-2xl text-gray-300 transition-colors hover:text-gray-900 sm:right-2 sm:top-2 sm:hidden"
+      >
+        <X />
+      </button>
+      <dialog
+        className="relative w-full max-w-6xl bg-transparent"
+        onClick={(e) => e.stopPropagation()}
+        open
+      >
         <button
           onClick={closeModal}
-          className="group absolute right-1 top-1 z-10 flex items-center justify-center rounded-full bg-gray-200/80 p-2 text-2xl text-gray-600 transition-colors hover:text-gray-900 sm:right-2 sm:top-2"
+          className="group absolute right-2 top-2 z-10 hidden items-center justify-center rounded-full bg-gray-200/80 p-2 text-2xl text-gray-600 transition-colors hover:text-gray-900 sm:right-2 sm:top-2 sm:flex"
         >
           <X />
         </button>
-        <div
-          onClick={(e) => e.stopPropagation()}
-          className="flex h-[90dvh] max-h-[1000px] flex-col items-center gap-x-4 overflow-auto rounded-lg bg-white p-1 pb-2 sm:flex-row sm:p-0 sm:pr-2"
-        >
+        <div className="flex h-[87dvh] max-h-[1000px] flex-col items-center gap-x-4 overflow-auto rounded-lg bg-white p-1 pb-2 sm:flex-row sm:p-0 sm:pr-2">
           <div className="relative h-full min-h-[400px] w-full sm:w-2/3 md:w-3/5">
             {loading && (
               <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
@@ -112,7 +119,7 @@ const DialogMenu = ({ menu, ids }: { menu: MenuItem; ids: string[] }) => {
             </div>
           </div>
         </div>
-        <div className="mt-2 flex items-center justify-center gap-8 sm:hidden">
+        {/* <div className="mt-2 flex items-center justify-center gap-8 sm:hidden">
           <ChevronLeft
             size={40}
             className="flex cursor-pointer items-center justify-center rounded-full bg-gray-200 p-2 text-2xl text-gray-600 transition-colors hover:text-gray-900"
@@ -123,8 +130,8 @@ const DialogMenu = ({ menu, ids }: { menu: MenuItem; ids: string[] }) => {
             className="flex cursor-pointer items-center justify-center rounded-full bg-gray-200 p-2 text-2xl text-gray-600 transition-colors hover:text-gray-900"
             onClick={handleNext}
           />
-        </div>
-        <div className="hidden sm:flex">
+        </div> */}
+        <div className="">
           <ChevronRight
             size={40}
             className="absolute -bottom-12 right-1/3 z-10 flex cursor-pointer items-center justify-center rounded-full bg-gray-200 p-2 text-2xl text-gray-600 transition-colors hover:text-gray-900 md:-right-12 md:top-1/2"
