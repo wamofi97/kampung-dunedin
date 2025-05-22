21dc68a8ee14e6760ea47a5eeea1d5b2635bb3f0
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-05-22 10:34:14 +0800

    fix sidedishes modal navigation

--- src/app/@modal/(.)menu/[id]/page.tsx
+++ src/app/@modal/(.)menu/[id]/page.tsx
@@ -13,8 +13,21 @@ interface MenuItem {
   altText: string;
 }
 
-const getFilteredMenuIds = async (filter: string | null) => {
-  const menus = await client.fetch(MENU_QUERY);
+const menuCategories = async () => {
+  const menu = await client.fetch(MENU_QUERY);
+  const mainDishes = menu.filter(
+    (item: MenuItem) => item.category === "Main Dishes",
+  );
+  const sideDishes = menu.filter(
+    (item: MenuItem) => item.category === "Side Dishes",
+  );
+
+  return { mainDishes, sideDishes };
+};
+
+const getMainDishesIds = async (filter: string | null) => {
+  const { mainDishes } = await menuCategories();
+  const menus = mainDishes;
   const filteredMenus = filter
     ? menus.filter((menu: MenuItem) => {
         if (filter === "rice") return menu.name.toLowerCase().includes("nasi");
@@ -36,6 +49,11 @@ const getFilteredMenuIds = async (filter: string | null) => {
   return filteredMenus.map((menu: MenuItem) => menu._id);
 };
 
+const getSideDishesIds = async () => {
+  const { sideDishes } = await menuCategories();
+  return sideDishes.map((menu: MenuItem) => menu._id);
+};
+
 const getMenu = async (id: string) => {
   const menu = await client.fetch(MENU_QUERY_BY_ID, { id });
   if (!menu) {
@@ -53,10 +71,10 @@ export default async function Page({
 }) {
   const id = (await params).id;
   const filter = (await searchParams).filter || null;
-  const [menu, ids] = await Promise.all([
-    getMenu(id),
-    getFilteredMenuIds(filter),
-  ]);
+  const menu = await getMenu(id);
+  const mainDishesIds = await getMainDishesIds(filter);
+  const sideDishesIds = await getSideDishesIds();
+  const ids = [...mainDishesIds, ...sideDishesIds];
 
   return <DialogMenu menu={menu} ids={ids} />;
 }
